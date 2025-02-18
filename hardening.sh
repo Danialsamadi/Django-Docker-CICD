f94218b0133f284847163a8252976e1bc9858d23
Danial Samadi <danisamadi11@gmail.com>
2025-02-18 18:56:19 -0500

    Add hardening.sh final edit

--- hardening.sh
+++ hardening.sh
@@ -5,6 +5,27 @@ DOMAIN=danialsamadi.github.io
 SSH_PORT=1245
 BAC_DIR=/opt/backup/file_$NOW
 
+if [-z $BAC_DIR]; then
+  echo "Already exist"
+else 
+  mikdir -p $BAC_DIR
+fi
 
-ls -la
 
+apt update && apt upgrade -y
+
+
+apt install curl vim fail2ban
+
+# disable and mask ufw
+systemctl stop ufw
+systemctl disable ufw
+systemctl mask ufw
+
+
+cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
+sed -i 's/ssh port/ ssh port=''$SSH_PORT/g' /etc/fail2ban.local
+
+systemctl restart fail2ban
+systemctl enable fail2ban
+fail2ban-client status
