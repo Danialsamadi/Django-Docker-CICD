f71dbda599b723d1c55f655984f43e74eb5105df
Danial Samadi <danisamadi11@gmail.com>
2025-02-19 15:02:38 -0500

    some changes on the hardening.sh file

--- hardening.sh
+++ hardening.sh
@@ -3,29 +3,36 @@
 # Variables
 DOMAIN=danialsamadi.github.io
 SSH_PORT=1245
+NOW=$(date +"%Y%m%d_%H%M%S")
 BAC_DIR=/opt/backup/file_$NOW
 
-if [-z $BAC_DIR]; then
-  echo "Already exist"
-else 
-  mikdir -p $BAC_DIR
+# Check if the backup directory already exists
+if [ -d "$BAC_DIR" ]; then
+  echo "Backup directory already exists: $BAC_DIR"
+else
+  mkdir -p "$BAC_DIR"
+  echo "Created backup directory: $BAC_DIR"
 fi
 
+# Update and upgrade the system
+sudo apt update && sudo apt upgrade -y
 
-apt update && apt upgrade -y
+# Install required packages
+sudo apt install -y curl vim fail2ban
 
 
-apt install curl vim fail2ban
-
-# disable and mask ufw
+# Disable and mask ufw
 systemctl stop ufw
 systemctl disable ufw
 systemctl mask ufw
 
-
+# Configure fail2ban
 cp /etc/fail2ban/fail2ban.conf /etc/fail2ban/fail2ban.local
-sed -i 's/ssh port/ ssh port=''$SSH_PORT/g' /etc/fail2ban.local
+sudo sed -i "s/#port = ssh/port = $SSH_PORT/g" /etc/fail2ban/jail.local
 
+# Restart and enable fail2ban
 systemctl restart fail2ban
 systemctl enable fail2ban
+
+# Check fail2ban status
 fail2ban-client status
